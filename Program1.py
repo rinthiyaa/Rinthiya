3933d9bbbcac17156bdbf62ecf7ae0e8517063a0
rinthiyaa <143588100+rinthiyaa@users.noreply.github.com>
2025-08-08 12:57:44 +0530

    Update Program1.py

--- Program1.py
+++ Program1.py
@@ -1,1 +1,25 @@
-
+# Problem - 1
+class calculater:
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
+    def caloperation(self,operation):
+        if operation == "add":
+            return self.a +self.b
+        elif operation == "sub":
+            return self.a - self.b
+        elif operation == "mul":
+            return self.a * self.b
+        elif operation == "div":
+            if self.b==0:
+                return "Cannot divisible by Zero"
+            else:
+                return self.a / self.b
+        else:
+            return "Invalid Input"
+a=float(input("Enter a number:"))
+b=float(input("Enter a Number:"))
+operation=input("Enter a operation (add,sub,mul,div):")
+new=calculater(a,b)
+output=new.caloperation(operation)
+print("Result:",output)
